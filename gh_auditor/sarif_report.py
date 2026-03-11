58b50d51a6b5444fa6215dca298a4085850039d4
AyushAggarwal1 <ayushaggarwal1136@gmail.com>
2026-03-11 11:48:41 +0530

    removed cache files

--- gh_auditor/sarif_report.py
+++ gh_auditor/sarif_report.py
@@ -477,12 +477,15 @@ def generate_sarif_report(report: dict) -> dict:
                 "invocations": [
                     {
                         "executionSuccessful": True,
-                        "workingDirectory": {
-                            "uri": report.get("audit_metadata", {}).get("organization", "")
+                        "properties": {
                             "organization": report.get("audit_metadata", {}).get("organization", ""),
                             "timestamp": report.get("audit_metadata", {}).get("timestamp", ""),
                             "totalReposScanned": report.get("audit_metadata", {}).get("total_repos_scanned", 0),
                             "totalWorkflowsScanned": report.get("audit_metadata", {}).get("total_workflows_scanned", 0),
+                        },
+                        "workingDirectory": {
+                            "uri": report.get("audit_metadata", {}).get("organization", ""),
+                            "organization": report.get("audit_metadata", {}).get("organization", ""),
                         },
                     }
                 ],
